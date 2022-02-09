f09ee403a54d342e67eb8a7d3d31f5579d26b31d
Raoji Patil <raojipatil22@gmail.com>
2022-02-09 10:54:36 +0530

    update

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,12 @@
 
 import './App.css';
-import {useState} from "react";
+
 function App() {
-  const [counter, setCounter] = useState([0]);
   return (
     <div className="App">
        <h1>My Portfolio</h1>
-      <h2>Raoji: {counter}</h2>
-   <button onClick={() => {
-     setCounter(counter +1); 
-   }}>counter</button>
+      <h2>Raoji Patil</h2>
+  
     </div>
   );
 }
